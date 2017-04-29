a5e57356247751df5c6b163fcb72eb7a549ac0f4
flyirian <flyirian@gmail.com>
2017-04-29 23:52:48 +0200

    ping forwarding parser initial

--- commands/forward/discord.py
+++ commands/forward/discord.py
@@ -3,6 +3,7 @@
 import time
 import discord
 import asyncio
+import re as _re
 
 import common.credentials.discord as _discord
 import common.logger as _logger
@@ -23,19 +24,84 @@ def start_discord(username, password, covername, handler, discord_queue):
     @client.event
     async def on_message(message):
 
-        now = time.localtime(None)
-        now_friendly = time.strftime("%Y-%m-%d @ %H:%M:%S %z", now)
-        header = '[' + str(covername) + ']'
-        header = header + '\t' + 'Time: {0}, covername: {1}'.format(str(now_friendly), covername)
-        header = header + '\n'
-        header = header + '----------' + '\n'
+        #now = time.localtime(None)
+        #now_friendly = time.strftime("%Y-%m-%d @ %H:%M:%S %z", now)
+        #header = '[' + str(covername) + ']'
+        #header = header + '\t' + 'Time: {0}, covername: {1}'.format(str(now_friendly), covername)
+        #header = header + '\n'
+        #header = header + '----------' + '\n'
 
         if message.mention_everyone:
-            body = 'FROM: {0}, DISCORD SERVER: {1}, CHANNEL: #{2} \n'.format(str(message.author),str(message.server), str(message.channel))
-            body = body + str(message.content).replace('@','#') + '\n'
-            discord_queue.put(header + body)
+            #body = 'FROM: {0}, DISCORD SERVER: {1}, CHANNEL: #{2} \n'.format(str(message.author),str(message.server), str(message.channel))
+            #body = body + str(message.content).replace('@','#') + '\n'
+            #discord_queue.put(header + body)
+
+            discord_queue.put(parse_message(str(covername), message))
 
     try:
         client.run(username, password)
     except Exception as error:
         _logger.log(prefix + 'Discord connection error: ' + str(error), _logger.LogLevel.ERROR)
+
+def parse_message(cover, message):
+    if cover == "fcon":
+        body = fcon_parser(message)
+    elif cover == "low effort horde spy":
+        body = horde_parser(message)
+    elif cover == "GOTG":
+        body = horde_parser(message)
+    elif cover == "brave_spy":
+        body = brave_spy(message)
+    else:
+        body = default_parser(message)
+
+    return "**[{0}]** | __{1}__\n```css\n{2}```"\
+        .format(cover, time.strftime("%H:%M:%S %z / %d-%m-%Y", time.localtime(None)), body)
+
+def default_parser(message):
+    return "FROM: {0}|{1}\nTO: {2}\n--------------------{3}"\
+        .format(str(message.author),str(message.server), str(message.channel), str(message.content).replace('@','#'))
+
+def fcon_parser(message):
+    try:
+        text = message.content.splitlines()
+
+        main_text = text[3].replace(" || ", "\n")
+
+        footer_text = text[-1].split(" ")
+
+        ping_from = footer_text[6]
+        ping_to = footer_text[8][1:-1]
+
+        return "FROM: {0}\nTO: {1}\n--------------------\n{2}" \
+            .format(ping_from, ping_to, main_text)
+
+    except Exception:
+        return default_parser(message)
+
+def gotg_parser(message):
+    try:
+        text = message.content.splitlines()
+
+        ping_from = text[1].split(" ", 1)[-1]
+        ping_to = text[2].split(" ", 1)[-1]
+
+        return "FROM: {0}|{1}\nTO: {2}\CHANNEL:{3}\n--------------------{4}" \
+            .format(ping_from, str(message.server), ping_to, str(message.channel),
+                    str(text[3:]).replace('@', '#'))
+    except Exception:
+        return default_parser(message)
+
+def horde_parser(message):
+    try:
+        text = message.content
+        raise NotImplementedError()
+    except Exception:
+        return default_parser(message)
+
+def brave_spy(message):
+    try:
+        text = message.content
+        raise NotImplementedError()
+    except Exception:
+        return default_parser(message)
