43ad2568f8795b8c0da69ba73f95ac81467b938b
Eric Gisse <jowr.pi@gmail.com>
2017-05-17 10:51:51 +0000

    pushed the authed request stuff into request_esi itself, as that makes more sense anyway

--- tri_api/endpoints/esi/passthrough.py
+++ tri_api/endpoints/esi/passthrough.py
@@ -6,10 +6,8 @@ def core_esi_passthrough(url):
 
     from flask import Flask, request, url_for, json, Response
     from common.api import base_url
-    import common.credentials.ldap as _ldap
     import common.request_esi
     import common.logger as _logger
-    import ldap
     import json
     import urllib
 
@@ -35,48 +33,17 @@ def core_esi_passthrough(url):
     for key in request.args:
         parameters[key] = request.args[key]
 
-    # the id parameter is just something i use internally
+    charid = parameters['charid']
     del(parameters['charid'])
 
     _logger.log('[' + __name__ + '] esi passthrough request for charid {0}: {1}'.format(request.args['charid'], url), _logger.LogLevel.DEBUG)
 
-    # snag the user's ldap token
-
-    ldap_conn = ldap.initialize(_ldap.ldap_host, bytes_mode=False)
-    try:
-        ldap_conn.simple_bind_s(_ldap.admin_dn, _ldap.admin_dn_password)
-    except ldap.LDAPError as error:
-        _logger.log('[' + __name__ + '] LDAP connection error: {}'.format(error),_logger.LogLevel.ERROR)
-        js = json.dumps({ 'error': 'internal ldap error'})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
-
-    try:
-        result = ldap_conn.search_s('ou=People,dc=triumvirate,dc=rocks', ldap.SCOPE_SUBTREE, filterstr='(&(objectclass=pilot)(uid={0}))'.format(charid), attrlist=['esiAccessToken'])
-        user_count = result.__len__()
-    except ldap.LDAPError as error:
-        _logger.log('[' + __name__ + '] unable to fetch ldap information: {}'.format(error),_logger.LogLevel.ERROR)
-        js = json.dumps({ 'error': 'internal ldap error'})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
-    # this shouldn't happen often tbh
-    if user_count == 0:
-        js = json.dumps({ 'error': 'no token for uid {}'.format(id)})
-        resp = Response(js, status=404, mimetype='application/json')
-        return resp
-
-    dn, atoken = result[0]
-    atoken = atoken['esiAccessToken'][0].decode('utf-8')
-
-    # add the token to the parameters
-
-    parameters['token'] = atoken
     parameterstring = urllib.parse.urlencode(parameters)
 
     # we're going to just pass the request through with the access token attached
 
     esi_url = base_url + url + '?' + parameterstring
-    print(esi_url)
-    code, result = common.request_esi.esi(__name__, esi_url, 'get')
+
+    code, result = common.request_esi.esi(__name__, esi_url, method='get', charid=charid)
     resp = Response(json.dumps(result), status=code, mimetype='application/json')
     return resp
