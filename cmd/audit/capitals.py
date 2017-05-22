c4635ccba093f2875ed9b5de8e7fc1eea09034aa
flyirian <flyirian@gmail.com>
2017-05-22 13:43:57 +0200

    update frsd commands

--- cmd/audit/capitals.py
+++ cmd/audit/capitals.py
@@ -91,6 +91,8 @@ class Capitals(_Command):
         print("Total Carriers: {0}\n Total Dreads: {1}\n Total FAX: {2}"
               .format(gc_count, gd_count, gf_count))
 
+        print("Out of {0} pilots, {1} do not meet the requirements.".format(users.__len__(), len(bad_users)))
+
         return kwargs
 
 
