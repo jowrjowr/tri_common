28e68b2f3a86408417be2a4a01380066553e6ddb
flyirian <flyirian@gmail.com>
2017-06-03 17:08:40 +0200

    group audit update

--- cmd/audit/groups.py
+++ cmd/audit/groups.py
@@ -105,9 +105,26 @@ class Corporation(_Command):
         member_id_list = [member['character_id'] for member in member_list]
         user_id_list = [int(user[1]['uid'][0].decode('utf-8')) for user in users]
 
-        missing_members = []
+        missing_members = list(set(member_id_list) - set(user_id_list))
 
-        print(set(member_id_list) - set(user_id_list))
+        # get character names from missing IDs
+        def get_member_names(ids):
+            from common.api import base_url
+            import common.request_esi
 
+            request_url = base_url + 'characters/names/{0}/?datasource=tranquility'.format('%2C'.join(str(ids)))
+            code, result = common.request_esi.esi(__name__, request_url, 'get')
 
+            if not code == 200:
+                # something broke severely
+                _logger.log('[' + __name__ + '] character/names API error for ids {0} ({1}: {2})'
+                            .format(', '.join(ids), code, result['error']),
+                            _logger.LogLevel.ERROR)
 
+                raise Exception
+
+            return result
+
+        missing_members_names = get_member_names(missing_members)
+
+        print("Missing Tokens: {0}".format(', '.join([char['character_name'] for char in missing_members_names])))
