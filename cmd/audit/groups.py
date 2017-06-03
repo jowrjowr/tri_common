71e2d93a7fa50f2e6ccb3fcdeb0cfa5b040fcef0
flyirian <flyirian@gmail.com>
2017-06-03 16:43:48 +0200

    group audit update

--- cmd/audit/groups.py
+++ cmd/audit/groups.py
@@ -103,11 +103,11 @@ class Corporation(_Command):
             return
 
         member_id_list = [member['character_id'] for member in member_list]
-        user_id_list = [user[1]['uid'] for user in users]
+        user_id_list = [int(user[1]['uid'][0].decode('utf-8')) for user in users]
 
         missing_members = []
 
-        print(user_id_list)
+        print(set(member_id_list) - set(user_id_list))
 
 
 
