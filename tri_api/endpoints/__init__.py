467504a9c37918136c7b8841ccd70d5401a09161
flyirian <flyirian@gmail.com>
2017-06-11 18:31:54 +0200

    character_id verify endpoint

--- tri_api/endpoints/__init__.py
+++ tri_api/endpoints/__init__.py
@@ -2,3 +2,5 @@ import tri_api.endpoints.esi.passthrough
 import tri_api.endpoints.core.jumpbridge
 import tri_api.endpoints.core.structures
 import tri_api.endpoints.core.isblue
+
+import tri_api.endpoints.verify
