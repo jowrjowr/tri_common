43c6a04fd24aa05f1b72032c7bb5e36fcd98904f
Eric Gisse <jowr.pi@gmail.com>
2017-06-28 19:41:41 +0000

    better discord user logging

--- commands/forward/discord.py
+++ commands/forward/discord.py
@@ -13,20 +13,20 @@ def start_discord(username, password, covername, handler, discord_queue):
 
     prefix = '[' + __name__ + '][' + username + '] '
     wait = random.randint(1,10)
-    _logger.log(prefix + 'starting up. waiting {0} seconds'.format(wait), _logger.LogLevel.INFO)
+    _logger.log(prefix + 'starting up. {0} ({1}) waiting {2} seconds'.format(covername, username, wait), _logger.LogLevel.INFO)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     client = discord.Client(loop=loop,cache_auth=False)
 
     @client.event
     async def on_ready():
-        _logger.log(prefix + 'discord connected', _logger.LogLevel.DEBUG)
+        _logger.log(prefix + 'discord user {0} ({1}) connected'.format(covername, username), _logger.LogLevel.INFO)
         loginmsg = '```diff' + '\n' + '+ {0} online```'.format(covername)
         discord_queue.put(loginmsg)
 
     @client.event
     async def on_server_remove():
-        _logger.log(prefix + 'discord disconnected', _logger.LogLevel.DEBUG)
+        _logger.log(prefix + 'discord user {0} ({1}) disconnected'.format(covername, username), _logger.LogLevel.INFO)
         loginmsg = '```diff' + '\n' + '+ {0} offline```'.format(covername)
         discord_queue.put(loginmsg)
 
@@ -34,12 +34,13 @@ def start_discord(username, password, covername, handler, discord_queue):
     async def on_message(message):
 
         if message.mention_everyone:
+            _logger.log(prefix + 'discord ping to user {0} ({1})'.format(covername, username), _logger.LogLevel.INFO)
             discord_queue.put(parse_message(str(covername), message))
 
     try:
         client.run(username, password)
     except Exception as error:
-        _logger.log(prefix + 'Discord connection error: ' + str(error), _logger.LogLevel.ERROR)
+        _logger.log(prefix + 'Discord connection error on user {0} ({1}): {2}'.format(covername, username, error), _logger.LogLevel.ERROR)
         loginmsg = '```diff' + '\n' + '- {0} offline```'.format(covername)
         discord_queue.put(loginmsg)
 
