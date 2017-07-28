58edd8ab44515ff64833a5a1572a8a48112de6bb
Eric Gisse <jowr.pi@gmail.com>
2017-07-28 04:35:54 +0000

    spaceaids scope

--- tri_core/common/scopes.py
+++ tri_core/common/scopes.py
@@ -6,7 +6,7 @@ scope += ['esi-corporations.read_corporation_membership.v1', 'esi-location.read_
 scope += ['esi-location.read_ship_type.v1', 'esi-skills.read_skillqueue.v1', 'esi-skills.read_skills.v1']
 scope += ['esi-universe.read_structures.v1', 'esi-corporations.read_structures.v1', 'esi-search.search_structures.v1']
 scope += ['esi-characters.read_corporation_roles.v1', 'esi-assets.read_assets.v1', 'esi-location.read_online.v1' ]
-
+scope += ['esi-characters.read_fatigue.v1'],
 # dedupe scope list because holy shit really?
 
 scope = set(scope)
