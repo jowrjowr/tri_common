1265e7100ef056184bb9a4fd00f09574c05226d6
Eric Gisse <jowr.pi@gmail.com>
2017-07-31 14:30:02 +0000

    blacklist endpoint

--- tri_api/endpoints/__init__.py
+++ tri_api/endpoints/__init__.py
@@ -7,6 +7,7 @@ import tri_api.endpoints.core.groups
 import tri_api.endpoints.core.broadcast
 import tri_api.endpoints.core.password
 import tri_api.endpoints.core.corpaudit
+import tri_api.endpoints.core.blacklist
 
 import tri_api.endpoints.auth
 import tri_api.endpoints.char
