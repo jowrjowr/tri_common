95f311b8e9b1f5bc6c74de4bf7e4950000cd8f05
flyirian <flyirian@gmail.com>
2017-08-02 20:41:14 +0200

    alliance audit debugging

--- tri_api/endpoints/core/allianceaudit.py
+++ tri_api/endpoints/core/allianceaudit.py
@@ -99,9 +99,6 @@ def core_allianceaudit(charid):
         if 'esiAccessToken' in entry and entry['esiAccessToken'] is not None and not entry['esiAccessToken'] == '':
             corp_dict['corps'][str(corp_id)]['tokens'] += 1
 
-        _logger.log('[' + __name__ + '] ' + entry['altOf'],
-                    _logger.LogLevel.DEBUG)
-
         if 'altOf' in entry:
             corp_dict['corps'][str(corp_id)]['mains'] += 0
         else:
