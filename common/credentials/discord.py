bdce072fafc1da4f35324d47aa1957c4522fd080
Eric Gisse <jowr.pi@gmail.com>
2017-08-02 14:20:17 +0000

    helper shit for spy hnting

--- common/credentials/discord.py
+++ common/credentials/discord.py
@@ -7,6 +7,9 @@ token = 'MjkwMTQ0MzMxOTY3MjM0MDU4.C6Wq7Q.c5B4veLpSStSafrC94WBb3UNUyo'
 class Channel(Enum):
     # a helpful enum because who the fuck knows what numbers mean
     ping_forwarding = 288786919175749632
+    counterintel = 341589867840405524
+    everyone = 269993133268271106
+    tri_administration = 269997361827151872
 
 # oauth stuff for spy registration
 # not currently the same as sovereign
