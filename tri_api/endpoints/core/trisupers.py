292736eed79fe4b273b072714840213a059673aa
flyirian <flyirian@gmail.com>
2017-08-04 00:11:40 +0200

    added super auditing to alliance auditing

--- tri_api/endpoints/core/trisupers.py
+++ tri_api/endpoints/core/trisupers.py
@@ -2,7 +2,7 @@ from flask import request
 from tri_api import app
 
 @app.route('/core/trisupers/', methods=[ 'GET' ])
-def core_trisupers(allianceid):
+def core_trisupers():
     from flask import request, Response
     from concurrent.futures import ThreadPoolExecutor, as_completed
     from common.check_role import check_role
