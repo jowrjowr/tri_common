c6d7bce9d4c795ef6a12e58d6858f5ac4fe2ebf4
flyirian <flyirian@gmail.com>
2017-08-03 20:37:32 +0200

    alliance audit debugging

--- tri_api/endpoints/core/allianceaudit.py
+++ tri_api/endpoints/core/allianceaudit.py
@@ -68,10 +68,12 @@ def core_allianceaudit(charid):
         resp = Response(js, status=500, mimetype='application/json')
         return resp
 
-    corp_dict = {'corps': {}, 'statistics': {}}
+    corp_dict = {'corps': {}, 'supers': {}}
 
     for cn in result:
         entry = result[cn]
+
+        # esi token audit
 
         corp_id = entry['corporation']
 
@@ -106,6 +108,100 @@ def core_allianceaudit(charid):
             corp_dict['corps'][str(corp_id)]['mains'] += 1
         else:
             corp_dict['corps'][str(corp_id)]['mains'] += 0
+
+        # supercapital audit
+
+        request_location_url = 'characters/{}/location/?datasource=tranquility'.format(entry['uid'])
+        esi_location_code, esi_location_result = common.request_esi.esi(__name__, request_location_url, method='get',
+                                                                        charid=entry['uid'])
+
+        request_ship_url = 'characters/{}/ship/?datasource=tranquility'.format(entry['uid'])
+        esi_ship_code, esi_ship_result = common.request_esi.esi(__name__, request_ship_url, method='get',
+                                                                charid=entry['uid'])
+        if esi_location_code != 200:
+            # something broke severely
+            _logger.log('[' + __name__ + '] location API error {0}: {1}'.format(code, esi_location_result['error']),
+                        _logger.LogLevel.ERROR)
+            error = esi_location_result['error']
+            err_result = {'code': code, 'error': error}
+            return code, err_result
+
+        if esi_ship_code != 200:
+            # something broke severely
+            _logger.log('[' + __name__ + '] ship API error {0}: {1}'.format(code, esi_ship_result['error']),
+                        _logger.LogLevel.ERROR)
+            error = esi_ship_result['error']
+            err_result = {'code': code, 'error': error}
+            return code, err_result
+
+        request_sys_url = 'universe/systems/{}/?datasource=tranquility'.format(esi_location_result['solar_system_id'])
+        esi_system_code, esi_system_result = common.request_esi.esi(__name__, request_sys_url, method='get')
+
+        if esi_system_code != 200:
+            # something broke severely
+            _logger.log('[' + __name__ + '] ship API error {0}: {1}'.format(code, esi_system_result['error']),
+                        _logger.LogLevel.ERROR)
+            error = esi_system_result['error']
+            err_result = {'code': code, 'error': error}
+            return code, err_result
+
+        # check ship
+        titans = {
+            11567: 'Avatar',
+            671: 'Erebus',
+            45649: 'Komodo',
+            3764: 'Leviathan',
+            42241: 'Molok',
+            23773: 'Ragnarok',
+        }
+        supers = {
+            23919: 'Aeon',
+            22852: 'Hel',
+            23913: 'Nyx',
+            3514: 'Revenant',
+            42125: 'Vendetta',
+            23917: 'Wyvern'
+        }
+
+        if esi_ship_result['ship_type_id'] in titans.keys():
+            corp_dict['supers'][entry['uid']] = {}
+            corp_dict['supers'][entry['uid']]['ship'] = titans[esi_ship_result['ship_type_id']]
+            corp_dict['supers'][entry['uid']]['type'] = "Titan"
+            corp_dict['supers'][entry['uid']]['active'] = True
+            corp_dict['supers'][entry['uid']]['location'] = esi_system_result['name']
+
+        elif esi_ship_result['ship_type_id'] in supers.keys():
+            corp_dict['supers'][entry['uid']] = {}
+            corp_dict['supers'][entry['uid']]['ship'] = supers[esi_ship_result['ship_type_id']]
+            corp_dict['supers'][entry['uid']]['active'] = True
+            corp_dict['supers'][entry['uid']]['location'] = esi_system_result['name']
+            corp_dict['supers'][entry['uid']]['type'] = "Supercarrier"
+        else:
+            request_assets_url = 'characters/{}/assets/?datasource=tranquility'.format(entry['uid'])
+            esi_assets_code, esi_assets_result = common.request_esi.esi(__name__, request_assets_url, method='get',
+                                                                        charid=entry['uid'])
+            if esi_assets_code != 200:
+                # something broke severely
+                _logger.log('[' + __name__ + '] asset API error {0}: {1}'.format(code, esi_assets_result['error']),
+                            _logger.LogLevel.ERROR)
+                error = esi_assets_result['error']
+                err_result = {'code': code, 'error': error}
+                return code, err_result
+
+            for asset in esi_assets_result:
+                if esi_ship_result['ship_type_id'] in titans.keys():
+                    corp_dict['supers'][entry['uid']] = {}
+                    corp_dict['supers'][entry['uid']]['ship'] = titans[esi_ship_result['ship_type_id']]
+                    corp_dict['supers'][entry['uid']]['type'] = "Titan"
+                    corp_dict['supers'][entry['uid']]['active'] = False
+                    corp_dict['supers'][entry['uid']]['location'] = esi_system_result['name']
+
+                elif esi_ship_result['ship_type_id'] in supers.keys():
+                    corp_dict['supers'][entry['uid']] = {}
+                    corp_dict['supers'][entry['uid']]['ship'] = supers[esi_ship_result['ship_type_id']]
+                    corp_dict['supers'][entry['uid']]['active'] = False
+                    corp_dict['supers'][entry['uid']]['location'] = esi_system_result['name']
+                    corp_dict['supers'][entry['uid']]['type'] = "Supercarrier"
 
     js = json.dumps(corp_dict)
     return Response(js, status=200, mimetype='application/json')
