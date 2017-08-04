faa79ca23d990e471424d4409bcacb5c3bf40c35
flyirian <flyirian@gmail.com>
2017-08-04 09:55:27 +0200

    added super auditing to alliance auditing

--- tri_api/endpoints/core/trisupers.py
+++ tri_api/endpoints/core/trisupers.py
@@ -130,12 +130,12 @@ def audit_pilot(entry):
                 raise Exception(error)
 
             if main_result is not None:
-                main = main_result.get('characterName', 'Unkown')
+                main = main_result.get('characterName', 'Unknown')
             else:
                 _logger.log(
                     '[' + __name__ + '] searching for main of {0} failed: {1}'.format(altOf, main_code),
                     _logger.LogLevel.ERROR)
-                main = 'Unkown'
+                main = 'Unknown'
         else:
             main = charname
 
