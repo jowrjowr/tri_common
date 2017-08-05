6228127b3b495b4ef5df4f249cc937b1abf802ba
flyirian <flyirian@gmail.com>
2017-08-05 18:41:45 +0200

    added corporation column to corp audit

--- tri_api/endpoints/core/corpaudit.py
+++ tri_api/endpoints/core/corpaudit.py
@@ -139,14 +139,27 @@ def fetch_chardetails(charid):
             _logger.log('[' + function + '] User corporationid does not exist: {0})'.format(charid), _logger.LogLevel.ERROR)
             charname = None
 
-
-
         chardetails['charname'] = charname
 
     else:
         (dn, info), = result.items()
 
         chardetails['charname'] = info['characterName']
+
+
+        try:
+            corp_id = result['corporation_id']
+
+            request_url = 'corporations/{0}/?datasource=tranquility'.format(corp_id)
+            code, result = common.request_esi.esi(__name__, request_url, 'get')
+
+            if not code == 200:
+                _logger.log('[' + function + '] /corporations API error {0}: {1}'.format(code, result['error']), _logger.LogLevel.WARNING)
+            else:
+                chardetails['corporation'] = result['corporation_name']
+        except KeyError as error:
+            _logger.log('[' + function + '] User corporationid does not exist: {0})'.format(charid), _logger.LogLevel.ERROR)
+            charname = None
 
         # does the char have a token?
 
