d270ab7dd1bdb750d45b8deb4af4460b1f723975
flyirian <flyirian@gmail.com>
2017-08-13 01:10:09 +0200

    trisupers stuff

--- tri_api/endpoints/core/trisupers.py
+++ tri_api/endpoints/core/trisupers.py
@@ -81,7 +81,7 @@ def core_trisupers():
 
 @app.route('/core/corpsupers/', methods=['GET'])
 def core_corpsupers():
-
+    dn = 'ou=People,dc=triumvirate,dc=rocks'
     # logging
 
     ipaddress = request.args.get('log_ip')
@@ -96,60 +96,52 @@ def core_corpsupers():
         resp = Response(js, status=405, mimetype='application/json')
         return resp
 
-    _logger.securitylog(__name__, 'corp audit information request', ipaddress=ipaddress, charid=charid)
+    _logger.securitylog(__name__, 'supers audit', ipaddress=ipaddress, charid=charid)
 
     # check for auth groups
 
-    corporation_id_list = []
-    character_id_list = []
+    allowed_roles = ['Director', 'Personnel_Manager']
+    code, result = check_role(__name__, charid, allowed_roles)
 
-    dn = 'ou=People,dc=triumvirate,dc=rocks'
-    code, char_result = _ldaphelpers.ldap_search(__name__, dn, '(&(|(uid={0})(altOf={0}))(esiAccessToken=*))'
-                                                 .format(charid), ['uid', 'corporation'])
+    if code == 'error':
+        error = 'unable to check character roles for {0}: ({1}) {2}'.format(charid, code, result)
+        _logger.log('[' + __name__ + ']' + error, _logger.LogLevel.ERROR)
+        js = json.dumps({'error': error})
+        resp = Response(js, status=500, mimetype='application/json')
+        return resp
+    elif code == False:
+        return Response(json.dumps({'error': "forbidden"}), status=403, mimetype='application/json')
 
-    if code == False:
-        error = 'failed to fetch characters for {0}: ({1}) {2}'.format(charid, code, char_result)
+    # get corp
+
+    code, result = _ldaphelpers.ldap_search(__name__, dn, '(uid={})'.format(charid), ['corporation'])
+
+    if code == 'error':
+        error = 'unable to check auth groups roles for {0}: ({1}) {2}'.format(charid, code, result)
         _logger.log('[' + __name__ + ']' + error, _logger.LogLevel.ERROR)
         js = json.dumps({'error': error})
         resp = Response(js, status=500, mimetype='application/json')
         return resp
 
-    for cn in char_result:
-        data = char_result[cn]
-        allowed_roles = ['Director', 'Personnel_Manager']
-        code, result = check_role(__name__, data['uid'], allowed_roles)
+    if result == None:
+        msg = 'charid {0} not in ldap'.format(charid)
+        _logger.log('[' + __name__ + '] {}'.format(msg), _logger.LogLevel.ERROR)
+        js = json.dumps({'error': msg})
+        return Response(js, status=404, mimetype='application/json')
 
-        if code == 'error':
-            error = 'unable to check character roles for {0}: ({1}) {2}'.format(data['uid'], code, result)
-            _logger.log('[' + __name__ + ']' + error, _logger.LogLevel.ERROR)
-            js = json.dumps({'error': error})
-            resp = Response(js, status=500, mimetype='application/json')
-            return resp
-        elif code == False:
-            pass
-        else:
-            _logger.log('[' + __name__ + '] sufficient roles to view corp auditing information', _logger.LogLevel.DEBUG)
+    (_, result), = result.items()
 
-            if data['corporation'] not in corporation_id_list:
-                request_url = 'corporations/{0}/members/?datasource=tranquility'.format(data['corporation'])
-                code, result = common.request_esi.esi(__name__, request_url, method='get', charid=data['uid'],
-                                                      version='v2')
+    # get super pilots
 
-                if not code == 200:
-                    # something broke severely
-                    _logger.log('[' + __name__ + '] corporations API error {0}: {1}'.format(code, result['error']),
-                                _logger.LogLevel.ERROR)
-                    error = result['error']
-                    result = {'code': code, 'error': error}
-                    return code, result
+    filterstr = '(&(esiAccessToken=*)(corporation={0}))'.format(result['corporation'])
+    attrlist = ['uid', 'corporation', 'characterName', 'altOf']
 
-                character_id_list += [char['character_id'] for char in result]
-                corporation_id_list.append(data['corporation'])
+    code_supers, result_supers = _ldaphelpers.ldap_search(__name__, dn, filterstr, attrlist)
 
     supers = dict()
 
-    with ThreadPoolExecutor(30) as executor:
-        futures = { executor.submit(audit_pilot_byid, user): user for user in character_id_list }
+    with ThreadPoolExecutor(75) as executor:
+        futures = { executor.submit(audit_pilot, result_supers[cn]): cn for cn in result_supers }
         for future in as_completed(futures):
             data = future.result()
 
@@ -294,151 +286,4 @@ def audit_pilot(entry):
 
     return ships
 
-def audit_pilot_byid(char_id):
-
-    ships = dict()
-    basic_pilot = dict()
-
-    uid = char_id
-
-    char_info = _esihelpers.character_info(char_id)
-
-    if char_info is None:
-        return None
-
-    _logger.log('[' + __name__ + '] auditing character ' + uid, _logger.LogLevel.DEBUG)
-
-    corpid = char_info['corporation_id']
-    charname = char_info['name']
-
-    # get altOf
-    dn = 'ou=People,dc=triumvirate,dc=rocks'
-    code, char_result = _ldaphelpers.ldap_search(__name__, dn, '(&(uid={0})(esiAccessToken=*))'
-                                                 .format(char_id), ['altOf'])
-
-    if not code:
-        error = 'failed to fetch characters for {0}: ({1}) {2}'.format(char_id, code, char_result)
-        _logger.log('[' + __name__ + ']' + error, _logger.LogLevel.ERROR)
-        js = json.dumps({'error': error})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
-
-    altOf = char_result['altOf']
-
-    # data pollution fix
-    if altOf == 'None': altOf = None
-
-    basic_pilot['uid'] = uid
-    basic_pilot['pilot'] = charname
-    basic_pilot['valid'] = False
-    basic_pilot['main_charid'] = altOf
-
-    corp_info = _esihelpers.corporation_info(corpid)
-
-    if corp_info is not None:
-        basic_pilot['corporation'] = corp_info['corporation_name']
-    else:
-        basic_pilot['corporation'] = "Unknown"
-
-    # hardcoded data for asset typeids
-    titans = {
-        11567: 'Avatar',
-        671: 'Erebus',
-        45649: 'Komodo',
-        3764: 'Leviathan',
-        42241: 'Molok',
-        23773: 'Ragnarok',
-    }
-    supers = {
-        23919: 'Aeon',
-        22852: 'Hel',
-        23913: 'Nyx',
-        3514: 'Revenant',
-        42125: 'Vendetta',
-        23917: 'Wyvern'
-    }
-
-
-    # fetch current ship
-
-    code, current_ship = _esihelpers.current_ship(uid)
-
-    if code == False:
-        return ships
-
-    # fetch super/titan typeids out of char assets
-
-    code, char_assets = _esihelpers.find_types(uid, list(titans) + list(supers))
-    if code == False:
-        return ships
-
-    # fetch character location
-
-    code, location = _esihelpers.char_location(uid)
-    if code == False:
-        return ships
-
-    # fetch main charname, if it exists
-
-    dn = 'ou=People,dc=triumvirate,dc=rocks'
-    filterstr = 'uid={}'.format(altOf)
-    attrlist = ['uid', 'characterName']
-
-    if not altOf == None:
-        result = _ldaphelpers.ldap_uid2name(__name__, altOf)
-        if result == None:
-            msg = 'failed to find main for {0}'.format(altOf)
-            _logger.log('[' + __name__ + ']' + msg, _logger.LogLevel.WARNING)
-        try:
-            main = result['characterName']
-        except:
-            main = "Unknown"
-    else:
-        main = charname
-
-    # does this character have a titan/super in assets?
-
-    for asset in char_assets:
-        asset_typeid = asset.get('type_id')
-        asset_id = asset.get('item_id')
-        if asset_typeid in list(titans) + list(supers):
-            ships[asset_id] = basic_pilot
-            ships[asset_id]['typeid'] = asset_typeid
-            ships[asset_id]['main_charname'] = main
-            ships[asset_id]['active'] = False
-            ships[asset_id]['location_id'] = asset['location_id']
-            ships[asset_id]['location_name'] = 'Unkown'
-
-        if asset_typeid in list(supers):
-            ships[asset_id]['type'] = supers[asset_typeid]
-            ships[asset_id]['class'] = "Supercarrier"
-        if asset_typeid in list(titans):
-            ships[asset_id]['type'] = titans[asset_typeid]
-            ships[asset_id]['class'] = "Titan"
-
-    # is this character flying a titan/super?
-    # this is last to override the asset search with the active super (if any)
-
-    active_typeid = current_ship.get('ship_type_id')
-    active_id = current_ship.get('ship_item_id')
-
-    if active_typeid in list(titans) + list(supers):
-        # setup basics
-        ships[active_id] = basic_pilot
-        ships[active_id]['typeid'] = active_typeid
-        ships[active_id]['main_charname'] = main
-        ships[active_id]['active'] = True
-        ships[active_id]['location_name'] = location['location']
-        # more complex location ids and names can be filled in later?
-
-    # actual ship specific shit
-    if active_typeid in list(titans):
-        ships[active_id]['type'] = titans[active_typeid]
-        ships[active_id]['class'] = "Titan"
-
-    if active_typeid in list(supers):
-        ships[active_id]['type'] = supers[active_typeid]
-        ships[active_id]['class'] = "Supercarrier"
-
-    return ships
 
