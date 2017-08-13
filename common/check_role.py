15deb4f0ce22e27e138529f5ec87327cd2573f1d
Eric Gisse <jowr.pi@gmail.com>
2017-08-13 00:09:25 +0000

    cleanup

--- common/check_role.py
+++ common/check_role.py
@@ -1,16 +1,15 @@
-def check_role(function, charid, roles):
+import common.logger as _logger
+import common.ldaphelpers as _ldaphelpers
+import common.request_esi
 
-    import common.logger as _logger
-    import common.request_esi
-    import common.credentials.ldap as _ldap
-    import ldap
+def check_role(function, charid, roles):
 
     # check to make sure that the user has the expected corp roles
 
     request_url = 'characters/{0}/roles/?datasource=tranquility'.format(charid)
-    code, result = common.request_esi.esi(__name__, request_url, method='get', charid=charid)
+    code, result = common.request_esi.esi(__name__, request_url, method='get', charid=charid, version='v1')
     if not code == 200:
-        error = 'unable to get character roles for {0}: ({1}) {2}'.format(id, code, result['error'])
+        error = 'unable to get character roles for {0}: ({1}) {2}'.format(charid, code, result['error'])
         _logger.log('[' + function + ']' + error,_logger.LogLevel.ERROR)
         return 'error', error
 
