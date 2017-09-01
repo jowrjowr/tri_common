4f20e96613e81c5f85088e59f37ae0c2f33d2f26
Eric Gisse <jowr.pi@gmail.com>
2017-09-01 15:48:35 +0000

    cleanup

--- maint/discord/refresh.py
+++ maint/discord/refresh.py
@@ -3,23 +3,24 @@
 def refresh_token(old_rtoken):
 
     from requests_oauthlib import OAuth2Session
-
+    import oauthlib.oauth2
     import common.credentials.discord as _discord
     import common.logger as _logger
-    import flask
 
-    base_url = 'https://discordapp.com/api'
-    token_url = '/oauth2/token'
-    base_auth_url = '/oauth2/authorize'
-    redirect = 'https://auth.triumvirate.rocks/5eyes/callback?server_type=discord'
+    client_id = _discord.client_id
+    client_secret = _discord.client_secret
+    redirect_url = _discord.redirect_url
+
+    token_url = base_url + '/oauth2/token'
+    base_auth_url = base_url + '/oauth2/authorize'
 
     extra = {
-        'client_id': _discord.client_id,
-        'client_secret': _discord.client_secret,
+        'client_id': client_id,
+        'client_secret': client_secret,
     }
 
     discord = OAuth2Session(
-        client_id=_discord.client_id,
+        client_id=client_id,
         token=old_rtoken,
     )
     try:
@@ -29,7 +30,11 @@ def refresh_token(old_rtoken):
             refresh_token=old_rtoken,
             **extra,
         )
-    except Exception as error:
-        return error
 
-    return result
+    except Exception as error:
+        # there are a rather large amount of exceptions
+
+        exception_type = type(error).__name__
+        return exception_type, error
+
+    return result, True
