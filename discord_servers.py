08a30da9b82956013f2a768ae0b45d3df6fd144b
Eric Gisse <jowr.pi@gmail.com>
2017-09-01 15:47:26 +0000

    central repo of known discord servers

---
+++ discord_servers.py
@@ -0,0 +1,57 @@
+# friendly discord id hardcodes
+
+f_discords = []
+
+f_discords.append(301016648339423232)   # avalanche
+f_discords.append(212890573156253696)   # wombo
+f_discords.append(275395882415816705)   # no mercy
+f_discords.append(233545865053077504)   # steel fury
+f_discords.append(322163331047882754)   # unofficial vg
+f_discords.append(283533833326821376)   # quam
+f_discords.append(269991543627055114)   # vg leadership
+f_discords.append(186667149060734976)   # nullify
+f_discords.append(110544661776453632)   # superflous wanderlust
+f_discords.append(192421707657641984)   # CO2
+
+# neutral discord hardcode. only useful for helping find an identity
+
+n_discords = []
+
+n_discords.append(243235450972536833)   # z-s overview
+n_discords.append(202724765218242560)   # /r/eve discord
+n_discords.append(108746777263415296)   # spectre fleet
+n_discords.append(285281772810403840)   # kugu
+n_discords.append(163975909580341249)   # scc lounge
+n_discords.append(222565109627617280)   # NETC
+n_discords.append(259950272065830912)   # pubg squad
+n_discords.append(81384788765712384)    # discord api
+n_discords.append(332880147810353153)   # book club
+n_discords.append(216398568049737728)   # weebfleet
+n_discords.append(299888109003472896)   # csm12
+n_discords.append(229744043737874434)   # zulu
+n_discords.append(336324139809767425)   # cap escalation [albion]
+n_discords.append(174613404005302272)   # talking in stations
+n_discords.append(296890122824187904)   # wspace pub
+n_discords.append(213691723329699841)   # ISD RUS
+n_discords.append(327311033771360256)   # crossout
+
+# explicitly hostile discords
+
+h_discords = []
+
+h_discords.append(212147332228055040)   # iron armada
+h_discords.append(280799069977444352)   # test renters (memefleet)
+h_discords.append(190646276256169994)   # imperium
+h_discords.append(97063100934541312)    # brave
+h_discords.append(122123963395604480)   # GOTG
+h_discords.append(306135559275937802)   # dreddit
+h_discords.append(336319028748615683)   # brand newbros (also test)
+h_discords.append(261264601503498261)   # IRC
+h_discords.append(282380059107393537)   # seraph's faggot den
+
+discords = dict()
+discords['friendly'] = f_discords
+discords['hostile'] = h_discords
+discords['neutral'] = n_discords
+
+
