b494c35790b0d892dea7d8b96a82f014d6b4ec18
Eric Gisse <jowr.pi@gmail.com>
2017-09-22 06:32:04 +0000

    eve token secret reset as per bartender

--- credentials/eve.py
+++ credentials/eve.py
@@ -1,6 +1,6 @@
 # live core credentials
 
 client_id = '17a0a2a4f5ab46b29b97be4296053f97'
-client_secret = 'r7pzEb0tIH2GXeCC2Wj7Kx2RbceCER1xfNLTnocz'
+client_secret = '95e666f9b85a0e70915895c4fabafc0fa82768cf'
 redirect_url = 'https://auth.triumvirate.rocks/eve/callback'
 
